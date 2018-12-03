1e08f8ac6c85249b7b73fe6fba076d51ebde3fdd
gallleggl <gabriela.l.gallego@vanderbilt.edu>
2018-12-02 21:26:18 -0600

    working!!!

--- app/src/main/java/com/example/gabygallego/udpcarcontrol2/MainActivity.java
+++ app/src/main/java/com/example/gabygallego/udpcarcontrol2/MainActivity.java
@@ -5,18 +5,25 @@ import android.graphics.Bitmap;
 import android.media.Image;
 import android.os.AsyncTask;
 
+import android.os.Handler;
 import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import org.opencv.android.OpenCVLoader;
+
 import android.util.Log;
+import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.SeekBar;
+
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
+import java.net.SocketTimeoutException;
 import java.text.NumberFormat;
+
 import org.opencv.*;
 import org.opencv.android.Utils;
 import org.opencv.core.CvException;
@@ -31,9 +38,16 @@ public class MainActivity extends AppCompatActivity {
     private static double turnData = 5.0;
     private static double accelerationData = 0.0;
     private static final int SERVER_PORT_DATA = 4597;
-    private static DatagramSocket mSocket_data,mSocket_image;
+    private static DatagramSocket mSocket_data, mSocket_image;
     private static final int SERVER_PORT_IMAGE = 4579;
-    private static final String SERVER_NAME = "10.67.155.142";
+    private static Handler h = new Handler();
+    private static Runnable runnable;
+    private static Mat defaultImage;
+    static {
+        if (BuildConfig.DEBUG) {
+            OpenCVLoader.initDebug();
+        }
+    }
 
 
     @Override
@@ -41,29 +55,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ImageView display = findViewById(R.id.imageView);
-
         try {
             establishSocket();
-            sendFake();
-
-        } catch (IOException ex){
+            defaultImage = Utils.loadResource(this,R.drawable.car,CV_LOAD_IMAGE_COLOR);
+        } catch (IOException ex) {
             //bad
         }
-        //seekBarReader();
-        new ImageReader(display,mSocket_image).execute("sup");
+        h.postDelayed(runnable = new Runnable() {
+            public void run() {
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        new ImageReader(mSocket_image, (ImageView) findViewById(R.id.imageView),defaultImage).execute();
+                    }
+                });
+                h.postDelayed(runnable, 330);
+            }
+        }, 330);
+        seekBarReader();
     }
 
-    private static void establishSocket() throws IOException{
+
+    private static void establishSocket() throws IOException {
         mSocket_data = new DatagramSocket(SERVER_PORT_DATA);
         mSocket_image = new DatagramSocket(SERVER_PORT_IMAGE);
     }
 
-    private static void sendFake() throws IOException {
-        byte[] buffer = "Hello".getBytes();
-        InetAddress mServerAddress = InetAddress.getByName(SERVER_NAME);
-        DatagramPacket messagePacket = new DatagramPacket(buffer, buffer.length, mServerAddress, SERVER_PORT_DATA);
-        mSocket_data.send(messagePacket);
-    }
 
     void seekBarReader() {
         SeekBar.OnSeekBarChangeListener seekBarChangeListener = new SeekBar.OnSeekBarChangeListener() {
@@ -76,16 +93,16 @@ public class MainActivity extends AppCompatActivity {
 
                 } else {
                     accelerationData = progress;
-                    accelerationData = (accelerationData*.05) + 15.5;
+                    accelerationData = (accelerationData * .05) + 15.5;
 
                 }
                 NumberFormat nf = NumberFormat.getInstance();
                 nf.setMaximumFractionDigits(2);
                 nf.setGroupingUsed(false);
-                message = "STEERING=" + turnData + ";";
-                message = message + "ACCELERATION=" + nf.format(accelerationData) + ";";
+                message = "PASSWORD=123456789;STEERING=" + turnData + ";";
+                message = message + "ACCELERATION=" + nf.format(accelerationData);
 
-                Log.d("TAG",message);
+                Log.d("TAG", message);
                 new DataSender(mSocket_data).execute(message);
             }
 
@@ -110,7 +127,7 @@ public class MainActivity extends AppCompatActivity {
 
 class DataSender extends AsyncTask<String, Void, String> {
     private static final int SERVER_PORT_DATA = 4597;
-    private static final String SERVER_NAME = "10.67.155.142";
+    private static final String SERVER_NAME = "10.66.22.111";
     private DatagramSocket mSocket_data;
 
     public DataSender(DatagramSocket mSocket_data) {
@@ -123,46 +140,69 @@ class DataSender extends AsyncTask<String, Void, String> {
             InetAddress mServerAddress = InetAddress.getByName(SERVER_NAME);
             DatagramPacket messagePacket = new DatagramPacket(buffer, buffer.length, mServerAddress, SERVER_PORT_DATA);
             mSocket_data.send(messagePacket);
-        } catch(IOException ex) {
-            //bad
+        } catch (IOException ex) {
+            Log.d("TAG","bad");
         }
         return message[0];
     }
 }
 
-class ImageReader extends AsyncTask<String,Void,Bitmap> {
-    private static final int BUFFER_SIZE = 256;
-    private DatagramSocket mSocket_image;
-    private ImageView display;
+class ImageReader extends AsyncTask<Void, Void, Void> {
+    DatagramSocket mSocket;
+    private static final int SERVER_PORT_IMAGE = 4579;
+    private static final String SERVER_NAME = "10.66.22.111";
+    private static Mat mRgba;
+    private static ImageView imageViewer;
+    private static Mat defaultImage;
 
-    public ImageReader(ImageView display, DatagramSocket mSocket_image) {
-        this.display = display;
-        this.mSocket_image = mSocket_image;
+    public ImageReader(DatagramSocket mSocket_image, ImageView imageViewer, Mat defaultImage) {
+        this.mSocket = mSocket_image;
+        this.imageViewer = imageViewer;
+        this.defaultImage = defaultImage;
     }
 
-    protected Bitmap doInBackground(String... message) {
-        Bitmap bm = null;
-        byte[] buffer = new byte[BUFFER_SIZE];
-        DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+    protected Void doInBackground(Void... message) {
         try {
-            mSocket_image.receive(packet);
-            Log.d("TAG","got image");
-        } catch (IOException ex){
+            String password = "PASSWORD=987654321";
+            byte[] buffer = password.getBytes();
+            InetAddress mServerAddress = InetAddress.getByName(SERVER_NAME);
+            DatagramPacket messagePacket = new DatagramPacket(buffer, buffer.length, mServerAddress, SERVER_PORT_IMAGE);
+            mSocket.send(messagePacket);
+            byte[] imageBytes = new byte[30000];
+            DatagramPacket packet = new DatagramPacket(imageBytes, imageBytes.length);
+
+            mSocket.setSoTimeout(1000);
+            try {
+                mSocket.receive(packet);
+                byte[] temporaryImageInMemory = packet.getData();
+                mRgba = Imgcodecs.imdecode(new MatOfByte(temporaryImageInMemory), Imgcodecs.CV_LOAD_IMAGE_COLOR);
+            } catch (SocketTimeoutException ex){
+                Log.d("TAG","exception");
+                mRgba = defaultImage;
+            }
+        } catch (IOException ex) {
             //bad
-            Log.d("TAG","Excpetion thrown");
+            Log.d("TAG", "Excpetion thrown");
         }
-        byte[] data = packet.getData();
-        Mat mat = Imgcodecs.imdecode(new MatOfByte(data),Imgcodecs.CV_LOAD_IMAGE_COLOR);
-        try {
-            bm = Bitmap.createBitmap(mat.cols(),mat.rows(),Bitmap.Config.ARGB_8888);
-            Utils.matToBitmap(mat,bm);
-        } catch (CvException ex) {
-            //bad
-        }
-        return bm;
+        return null;
     }
 
-    protected void onPostExecute(Bitmap bm ) {
-        display.setImageBitmap(bm);
+    @Override
+    protected void onPostExecute(Void result) {
+        //do stuff
+        try {
+            loadImage();
+        } catch (IOException e) {
+            Log.d("Error", "IO Exception Loading Image");
+        }
     }
-}
+
+    protected void loadImage() throws IOException {
+        // convert MAT to a bitmap
+        Bitmap bm = Bitmap.createBitmap(mRgba.cols(), mRgba.rows(), Bitmap.Config.ARGB_8888);
+        Utils.matToBitmap(mRgba, bm);
+        //setting the image
+        imageViewer.setImageBitmap(bm);
+    }
+};
+
